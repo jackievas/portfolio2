d047b31eed99b56e721e763616025fb72ed8224d
jackievas <145609624+jackievas@users.noreply.github.com>
2023-11-20 16:04:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', async function () {
     try {
-        // Fetch JSON data (employees.json)
+        console.log('Fetching JSON data...');
         const responseJson = await fetch('employees.json');
         if (!responseJson.ok) {
             throw new Error(`Error fetching JSON: ${responseJson.status} ${responseJson.statusText}`);
@@ -10,9 +10,9 @@ document.addEventListener('DOMContentLoaded', async function () {
         // Assuming your JSON has a property called 'name'
         const name = jsonData[0].name;
 
+        console.log('Displaying information...');
         document.body.innerHTML = `<p>Name: ${name}</p>`;
     } catch (error) {
         console.error('Error fetching or parsing JSON:', error.message);
     }
 });
-
