2b410b1ad79ecdc11e81829975fd37337221cb37
jackievas <145609624+jackievas@users.noreply.github.com>
2023-11-20 15:49:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,41 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    try {
-        const jsonData = await fetchData('employees.json');
-        const xmlData = await fetchData('employees.xml', 'text');
-        const xmlDoc = parseXml(xmlData);
-        const positionsData = await fetchData('positions.json');
+// Simple JavaScript script to fetch data from employees.json and display employee names
 
-        const employees = mergeData(jsonData, xmlDoc, positionsData);
-
-        displayEmployees(employees);
-
-        console.log('Script ran successfully!');
-    } catch (error) {
-        handleFetchError(error);
-    }
-});
-
-async function fetchData(url, type = 'json') {
+async function fetchData(url) {
     const response = await fetch(url);
     if (!response.ok) {
-        throw new Error(`Error fetching ${type.toUpperCase()}: ${response.status} ${response.statusText}`);
+        throw new Error(`Error fetching JSON: ${response.status} ${response.statusText}`);
     }
-    return type === 'json' ? await response.json() : await response.text();
+    return await response.json();
 }
 
-function parseXml(xmlText) {
-    return new DOMParser().parseFromString(xmlText, 'application/xml');
+function extractEmployeeNames(jsonData) {
+    return jsonData.map(employee => employee.name);
 }
 
-function mergeData(jsonData, xmlDoc, positionsData) {
-    return jsonData.map(jsonEmployee => {
-        const employeeId = jsonEmployee.id;
-        const xmlEmployee = xmlDoc.querySelector(`employee[id="${employeeId}"]`);
-
-        if (xmlEmployee) {
-            const position = jsonEmployee.position;
-            const positionInfo = positionsData.positions[position];
-
-            if (positionInfo) {
-                return {
-                    id: employeeId,
-                    name: jsonEmployee.name,
-                    position,
-                    department: xmlEmployee.getAttribute('department'),
-                    positionDescription: positionInfo.description,
-                    positionSkills: positionInfo.skills
-                };
-            }
-        }
-        return null;
-    }).filter(Boolean);
-}
-
-function displayEmployees(employees) {
-    const employeeListContainer = document.getElementById('employeeList');
-
-    employees.forEach(employee => {
-        const employeeCard = document.createElement('div');
-        employeeCard.classList.add('employee-card');
-        employeeCard.innerHTML = `
-            <h3>${employee.name}</h3>
-            <p><strong>Position:</strong> ${employee.position}</p>
-            <p><strong>Department:</strong> ${employee.department}</p>
-            <p><strong>Position Description:</strong> ${employee.positionDescription}</p>
-            <p><strong>Position Skills:</strong> ${employee.positionSkills.join(', ')}</p>
-        `;
-        employeeListContainer.appendChild(employeeCard);
+function displayEmployeeNames(names) {
+    names.forEach(name => {
+        console.log(name);
     });
 }
 
-function handleFetchError(error) {
-    console.error('An error occurred:', error);
+async function main() {
+    try {
+        const jsonData = await fetchData('employees.json');
+        const employeeNames = extractEmployeeNames(jsonData);
 
-    if (error instanceof SyntaxError) {
-        console.error('This is a syntax error. Double-check your JSON or XML files for any formatting issues.');
-    } else {
-        console.error('Please check your code and data files for any issues.');
+        displayEmployeeNames(employeeNames);
+
+        console.log('Script ran successfully!');
+    } catch (error) {
+        console.error('An error occurred:', error);
+
+        if (error instanceof SyntaxError) {
+            console.error('This is a syntax error. Double-check your JSON file for any formatting issues.');
+        } else {
+            console.error('Please check your code and data file for any issues.');
+        }
     }
 }
+
+// Call the main function
+main();
