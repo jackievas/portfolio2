6fcfd4d841adf53514967790e08d22981973dda4
jackievas <145609624+jackievas@users.noreply.github.com>
2023-11-20 15:00:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Fetch JSON data (employees.json)
-    fetch('employees.json')
-        .then(response => response.json())
-        .then(jsonData => {
-            // Fetch XML data (employees.xml)
-            fetch('employees.xml')
-                .then(response => response.text())
-                .then(xmlData => {
-                    // Parse XML data
-                    const parser = new DOMParser();
-                    const xmlDoc = parser.parseFromString(xmlData, 'application/xml');
+document.addEventListener('DOMContentLoaded', async function () {
+    try {
+        // Fetch JSON data (employees.json)
+        const responseJson = await fetch('employees.json');
+        if (!responseJson.ok) {
+            throw new Error(`Error fetching JSON: ${responseJson.status} ${responseJson.statusText}`);
+        }
+        const jsonData = await responseJson.json();
 
-                    // Fetch JSON data for positions
-                    fetch('positions.json')
-                        .then(response => response.json())
-                        .then(positionsData => {
-                            // Combine JSON and XML data
-                            const employees = mergeData(jsonData, xmlDoc, positionsData);
+        // Fetch XML data (employees.xml)
+        const responseXml = await fetch('employees.xml');
+        if (!responseXml.ok) {
+            throw new Error(`Error fetching XML: ${responseXml.status} ${responseXml.statusText}`);
+        }
+        const xmlText = await responseXml.text();
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlText, 'application/xml');
 
-                            // Display employees
-                            displayEmployees(employees);
-                        })
-                        .catch(error => console.error('Error fetching positions JSON:', error));
-                })
-                .catch(error => console.error('Error fetching XML:', error));
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
+        // Fetch JSON data for positions
+        const responsePositions = await fetch('positions.json');
+        if (!responsePositions.ok) {
+            throw new Error(`Error fetching positions JSON: ${responsePositions.status} ${responsePositions.statusText}`);
+        }
+        const positionsData = await responsePositions.json();
+
+        // Combine JSON, XML, and positions data
+        const employees = mergeData(jsonData, xmlDoc, positionsData);
+
+        // Display employees
+        displayEmployees(employees);
+    } catch (error) {
+        console.error(error.message);
+    }
 });
 
 function mergeData(jsonData, xmlDoc, positionsData) {
-    // Combine JSON, XML, and positions data based on a common identifier (e.g., employee ID)
     const mergedData = [];
 
     jsonData.forEach(jsonEmployee => {
         const employeeId = jsonEmployee.id;
-
         const xmlEmployee = xmlDoc.querySelector(`employee[id="${employeeId}"]`);
+
         if (xmlEmployee) {
             const position = jsonEmployee.position;
             const positionInfo = positionsData[position];
@@ -46,7 +50,6 @@ function mergeData(jsonData, xmlDoc, positionsData) {
                     name: jsonEmployee.name,
                     position: position,
                     department: xmlEmployee.getAttribute('department'),
-                    // Add more fields as needed
                     positionDescription: positionInfo.description,
                     positionSkills: positionInfo.skills
                 };
@@ -71,7 +74,6 @@ function displayEmployees(employees) {
             <p><strong>Department:</strong> ${employee.department}</p>
             <p><strong>Position Description:</strong> ${employee.positionDescription}</p>
             <p><strong>Position Skills:</strong> ${employee.positionSkills.join(', ')}</p>
-            <!-- Add more fields as needed -->
         `;
         employeeListContainer.appendChild(employeeCard);
     });
