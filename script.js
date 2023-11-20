8bce92bf74bb74c379f7c3248de4130f2a329962
jackievas <145609624+jackievas@users.noreply.github.com>
2023-11-20 15:56:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,8 @@
-// Simple JavaScript script to fetch data from employees.json and display employee names
+const jsonString = '{"id": 1, "name": "John Doe", "position": "Software Engineer"}';
 
-async function fetchData(url) {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(`Error fetching JSON: ${response.status} ${response.statusText}`);
-    }
-    return await response.json();
+try {
+    const jsonData = JSON.parse(jsonString);
+    console.log('Parsed JSON:', jsonData);
+} catch (error) {
+    console.error('Error parsing JSON:', error.message);
 }
-
-function extractEmployeeNames(jsonData) {
-    return jsonData.map(employee => employee.name);
-}
-
-function displayEmployeeNames(names) {
-    names.forEach(name => {
-        console.log(name);
-    });
-}
-
-async function main() {
-    try {
-        const jsonData = await fetchData('employees.json');
-        const employeeNames = extractEmployeeNames(jsonData);
-
-        displayEmployeeNames(employeeNames);
-
-        console.log('Script ran successfully!');
-    } catch (error) {
-        console.error('An error occurred:', error);
-
-        if (error instanceof SyntaxError) {
-            console.error('This is a syntax error. Double-check your JSON file for any formatting issues.');
-        } else {
-            console.error('Please check your code and data file for any issues.');
-        }
-    }
-}
-
-// Call the main function
-main();
