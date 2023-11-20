f0b201b0105d4d5f8ede1e8e0d8f8723b108f8a4
jackievas <145609624+jackievas@users.noreply.github.com>
2023-11-20 16:07:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,44 @@ document.addEventListener('DOMContentLoaded', async function () {
         }
         const jsonData = await responseJson.json();
 
-        // Assuming your JSON has a property called 'name'
-        const name = jsonData[0].name;
+        // Fetch JSON data for positions
+        const responsePositions = await fetch('positions.json');
+        if (!responsePositions.ok) {
+            throw new Error(`Error fetching positions JSON: ${responsePositions.status} ${responsePositions.statusText}`);
+        }
+        const positionsData = await responsePositions.json();
 
         console.log('Displaying information...');
-        document.body.innerHTML = `<p>Name: ${name}</p>`;
+        displayEmployeeData(jsonData, positionsData);
     } catch (error) {
-        console.error('Error fetching or parsing JSON:', error.message);
+        console.error('An error occurred:', error);
+
+        if (error instanceof SyntaxError) {
+            console.error('This is a syntax error. Double-check your JSON files for any formatting issues.');
+        } else {
+            console.error('Please check your code and data files for any issues.');
+        }
     }
 });
+
+function displayEmployeeData(jsonData, positionsData) {
+    const employeeListContainer = document.getElementById('employeeList');
+
+    jsonData.forEach(employee => {
+        const position = employee.position;
+        const positionInfo = positionsData[position];
+
+        if (positionInfo) {
+            const listItem = document.createElement('div');
+            listItem.classList.add('employee-card');
+            listItem.innerHTML = `
+                <h3>${employee.name}</h3>
+                <p><strong>Position:</strong> ${position}</p>
+                <p><strong>Department:</strong> ${employee.department}</p>
+                <p><strong>Position Description:</strong> ${positionInfo.description}</p>
+                <p><strong>Position Skills:</strong> ${positionInfo.skills.join(', ')}</p>
+            `;
+            employeeListContainer.appendChild(listItem);
+        }
+    });
+}
